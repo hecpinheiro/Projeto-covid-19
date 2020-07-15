a372eca866c261397598af710c60c15f1c3bafbf
hecpinheiro <hectorspinheiro@gmail.com>
2020-07-15 18:48:32 -0300

    Esboço da tela inicial

---
+++ src/pages/main/index.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react'
+import api from '../../services/api'
+import './styles.css'
+
+export default class Main extends Component {
+    state = {
+        datauf: []
+    }
+    
+    componentDidMount() {
+        this.loadData()
+    }
+
+    loadData = async () => {
+        const response = await api.get('/api/report/v1')
+        console.log(response.data.data)
+
+        this.setState({datauf: response.data.data})
+    }
+    
+    
+    render() {
+        return (
+            <div className = "itens-list">
+                {this.state.datauf.map(item => (
+                
+                <article key={item.uid}>
+                    <strong>{item.uf}</strong>
+                    <p>{item.state}</p>
+                    
+                    <a href="">Mostrar</a>
+
+
+                </article>
+                
+                ))}
+            </div>
+        )
+    }
+}
