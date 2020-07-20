c65f4250a4efd354d55827764dbf60731cb36554
hecpinheiro <hectorspinheiro@gmail.com>
2020-07-20 00:12:57 -0300

    Att paleta de cores e esboço do gráfico

--- src/pages/statesbr/index.js
+++ src/pages/statesbr/index.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react'
 import api from '../../services/api'
 import "./styles.css"
 
+import {XYPlot, LineSeries} from 'react-vis';
+
 export default class Statesbr extends Component {
     state = {
         datauf:{}
@@ -19,13 +21,32 @@ export default class Statesbr extends Component {
     render() {
         const {datauf} = this.state
 
+        const data = [
+            {x: 0, y: 8},
+            {x: 1, y: 8},
+            {x: 2, y: 8},
+            {x: 3, y: 8},
+            {x: 4, y: 8},
+            {x: 5, y: 8},
+            {x: 6, y: 6},
+            {x: 7, y: 3},
+            {x: 8, y: 2},
+            {x: 9, y: 0}
+          ]
+
         return(
             <div className="states-info">
                 <h1>{datauf.state}</h1>
                 <p>Casos confirmados: {datauf.cases}</p>
                 <p class="deaths">Mortes: {datauf.deaths}</p>
                 <p>Atualizado em {datauf.datetime}</p>
+
+                <XYPlot height={300} width={300}>
+                <LineSeries data={data} />
+                </XYPlot>
             </div>
+
+            
         ) 
     }
 }
